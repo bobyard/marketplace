e502071399c3413827125404c2dd0c77b6516901
DivenYoung <DivenYoungth@gmail.com>
2023-03-01 21:01:16 +0800

    style: custom connect-button style

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import '@suiet/wallet-kit/style.css';
 import './suiet-wallet-kit-custom.css'
 import Header from "./Header";
 import {WalletProvider} from "@suiet/wallet-kit";
